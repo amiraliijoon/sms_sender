3a68d465bd6bf76e5b6c7bcaabe2c10a8896d1ba
amirali <pourrajabiamirali@gmail.com>
2024-11-06 16:59:14 +0330

    start with simple calculate

---
+++ calculator.py
@@ -0,0 +1,24 @@
+#start
+
+x = float(input("enter your first number: "))
+
+
+y = float(input("enter your second number: "))
+
+
+z = str(input("what are you going to do? "))
+
+if (z =="*"):
+    print(x*y)
+
+elif (z == "+"):
+    print(x+y)
+
+elif (z == "-"):
+    print(x-y)
+
+elif (z == "/"):
+    print(x/y)
+
+elif (z == "%"):
+    print(x%y)
